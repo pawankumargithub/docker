ac4a4035bcb86bc0f8067bc6ba966c09c06db85e
pkgupta789428@gmail.com <pkgupta789428@gmail.com>
2023-03-16 22:56:42 -0700

    first commit

---
+++ src/main/java/com/docker/DockerDemo1Application.java
@@ -0,0 +1,13 @@
+package com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemo1Application.class, args);
+	}
+
+}
